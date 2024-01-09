454fa45ba4418b9a572dcb2463302aeac7308b0a
Edward Nguyen <eddy-nguyen@hotmail.com>
2024-01-09 17:24:13 -0500

    Added netlify functions

--- apps/budget-app-web/server/netlify/functions/api.js
+++ apps/budget-app-web/server/netlify/functions/api.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
-require("dotenv").config({ path: "./config.env" });
+// require("dotenv").config({ path: "./config.env" });
+const serverless = require("serverless-http");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/api"));
+app.use(require("../../routes/api"));
 
 app.use(express.static("public"));
 
@@ -18,3 +19,5 @@ app.listen(port, () => {
   //  });
   console.log(`Server is running on port: ${port}`);
 });
+module.exports = app;
+module.exports.handler = serverless(app);
